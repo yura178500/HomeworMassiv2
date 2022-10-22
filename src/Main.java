3c6394700e04d8e56b079822eb9d68f895d61ba9
yura178500 <yura178500@yandex.ru>
2022-10-22 19:20:18 +0300

    Задача 2 Поправил

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
 public class Main {
-    private static int minAmount;
-
-    public static void main(String[] args) {
+        public static void main(String[] args) {
 
         int[] amount = generateRandomArray();
     }
@@ -9,21 +7,22 @@ public class Main {
     public static int[] generateRandomArray() {
         java.util.Random random = new java.util.Random();
         int[] amount = new int[30];
+        int maxAmount = 0;
+        int minAmount = 0;
         for (int index = 0; index < amount.length; index++) {
             amount[index] = random.nextInt(100_000) + 100_000;
-            int minAmount = 0;
-            int maxAmount = 0;
             minAmount = amount[index];
-            maxAmount = minAmount;
-            for (index = 1; index < amount.length; ++index) {
-                if (amount[index] > maxAmount) maxAmount = amount[index];
-                if (amount[index] < minAmount) minAmount = amount[index];
-            }
-            System.out.println("Максимальная сумма трат за день составила   " + maxAmount);
-            System.out.println("Минимальная сумма трат за день составила   " + minAmount);
+            //maxAmount = minAmount;
+            //for (index = 1; index < amount.length; ++index) {
+            if (amount[index] > maxAmount) maxAmount = amount[index];
+            if (amount[index] < minAmount) minAmount = amount[index];
         }
+        System.out.println("Максимальная сумма трат за день составила   " + maxAmount);
+        System.out.println("Минимальная сумма трат за день составила   " + minAmount);
         return amount;
-    }
-}
+    }}
+
+
+
 
 
